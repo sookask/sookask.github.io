b9ebd896d7d413c11788e4b6338b352dee4ddab9
macm0d <128816270+sookask@users.noreply.github.com>
2025-06-06 10:42:51 +0300

    3xl->4xl

--- app/dfu/page.tsx
+++ app/dfu/page.tsx
@@ -17,7 +17,7 @@ export default function DFUPage() {
 
 	return (
 		<div className="container px-4 md:px-6 py-12">
-			<div className="flex flex-col gap-8 max-w-3xl mx-auto">
+			<div className="flex flex-col gap-8 max-w-4xl mx-auto">
 
 				{/* Hero */}
 				<motion.div
